6442e890381d76dae494e28fcf07010dc2580bfc
Stuart Haas <stuart.haas@fusionary.com>
2020-09-19 00:50:46 -0400

    Docker compose config improvements, fix nginx config

--- client/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $1 == "production" ] ; then
-  yarn build
-else
-  yarn start
-fi
--- server/scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $1 == "production" ] ; then
-  yarn start
-else
-  npm install -g nodemon && yarn dev
-fi
