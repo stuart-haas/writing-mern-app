f1608fa1b5671e92ad8c080fc28fb7416434b254
Stuart Haas <stuart.haas@fusionary.com>
2020-09-18 21:52:20 -0400

    Add docker compose dev file

--- client/scripts/start.sh
+++ client/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 == "production" ] ; then
-  yarn start
+  yarn build
 else
-  yarn dev
+  yarn start
 fi
--- client/webpack.config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-
-module.exports = {
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: 3000
-  }
-};
