d69f02581029138bafd8da5f8c7891e8ab5b23e1
Stuart Haas <stuart.haas@fusionary.com>
2020-10-05 22:07:48 -0400

    Config adjustments

--- .scripts/setup.js
+++ .scripts/setup.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process')
 const cliSelect = require('cli-select')
 const chalk = require('chalk')
-const figlet = require('figlet')
 
 const options = {
     values: ['dev', 'prod'],
@@ -15,24 +14,15 @@ const options = {
     }
 }
 
-figlet('FUSIONARY', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-    console.log(chalk.bold('Choose Docker Config:'))
-
-    cliSelect(options).then((response) => {
-        const cmd = exec(`yarn docker:${response.value}`)
-        cmd.stdout.on('data', (data) => {
-            console.log(data)
-        })
-        cmd.stderr.on('data', (data) => {
-            console.error(data)
-        })
-    }).catch(() => {
-        console.log('Bye!')
+console.log(chalk.bold('Choose Docker Config:'))
+cliSelect(options).then((response) => {
+    const cmd = exec(`yarn docker:${response.value}`)
+    cmd.stdout.on('data', (data) => {
+        console.log(data)
     })
+    cmd.stderr.on('data', (data) => {
+        console.error(data)
+    })
+}).catch(() => {
+    console.log('Bye!')
 })
