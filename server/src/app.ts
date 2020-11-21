391513729151de26200dd147d029fa7cf8c1fd34
Stuart Haas <stuart.haas@fusionary.com>
2020-11-20 21:55:29 -0500

    Add helmet middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
@@ -45,6 +46,7 @@ export default class App {
   }
 
   private useMiddlewares() {
+    this.app.use(helmet());
     this.app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
